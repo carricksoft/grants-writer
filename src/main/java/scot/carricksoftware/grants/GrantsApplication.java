b8b088deef616e7c0401c88248c74d36fe0f2ec0
Andrew Grant <carricksoft@gmail.com>
2025-04-01 15:39:18 +0100

    REset again

---
+++ src/main/java/scot/carricksoftware/grants/GrantsApplication.java
@@ -0,0 +1,13 @@
+package scot.carricksoftware.grants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrantsApplication.class, args);
+	}
+
+}

2c0e7525a9296bae8f310f35c7724b8882630705
Andrew Grant <carricksoft@gmail.com>
2025-04-08 16:40:41 +0100

    liquibase testing

--- src/main/resources/db/changelog/migration/_0.2.0_migrate.sql
+++
@@ -1,8 +0,0 @@
--- liquibase formatted sql
--- changeset zuhaimi:1677359720411-3
-CREATE TABLE `department` (
-    id BIGINT NOT NULL,
-    name VARCHAR(255),
-    CONSTRAINT `departmentP` PRIMARY KEY (id)
-);
-

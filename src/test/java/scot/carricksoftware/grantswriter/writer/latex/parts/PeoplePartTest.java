96e1e4c043230482198dba79d2fdd8c76feb23d6
Andrew Grant <carricksoft@gmail.com>
2025-06-07 17:32:03 +0100

    PeoplePart write test

--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/PeoplePartTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/PeoplePartTest.java
@@ -10,10 +10,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.domains.people.Person;
 import scot.carricksoftware.grantswriter.services.people.PersonService;
 import scot.carricksoftware.grantswriter.writer.latex.sections.PersonSection;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static scot.carricksoftware.grantswriter.GenerateRandomPeopleValues.GetRandomPerson;
 
 @ExtendWith(MockitoExtension.class)
 class PeoplePartTest {
@@ -33,7 +41,17 @@ class PeoplePartTest {
     }
 
     @Test
-    void constructorTest() {
-        assertNotNull(peoplePart);
+    void writeTest() {
+        List<Person> people = new ArrayList<>();
+        int limit = 10;
+        for (int i = 0; i < limit; i++) {
+            Person person = GetRandomPerson();
+            people.add(person);
+        }
+
+        when(personServiceMock.findAll()).thenReturn(people);
+
+        peoplePart.write();
+        verify(personSectionMock, times(limit)).write(any());
     }
 }

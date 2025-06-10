a34a753b85be29fa7c8d92d8e65488c86ecd37dd
Andrew Grant <carricksoft@gmail.com>
2025-06-10 11:40:06 +0100

    PersonSubSectionReferencesWriterTest

--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/sections/PersonSubSectionReferencesWriterTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/sections/PersonSubSectionReferencesWriterTest.java
@@ -7,11 +7,15 @@ package scot.carricksoftware.grantswriter.writer.latex.sections;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.domains.people.Person;
 import scot.carricksoftware.grantswriter.writer.latex.LatexSubSectionHeader;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class PersonSubSectionReferencesWriterTest {
 
     private PersonSubSectionReferencesWriter writer;
@@ -19,13 +23,17 @@ class PersonSubSectionReferencesWriterTest {
     @Mock
     private LatexSubSectionHeader latexSubSectionHeaderMock;
 
+    @Mock
+    private Person personMock;
+
     @BeforeEach
     void setUp() {
         writer = new PersonSubSectionReferencesWriterImpl(latexSubSectionHeaderMock);
     }
 
     @Test
-    void constructorTest() {
-        assertNotNull(writer);
+    void writeTest() {
+        writer.write(personMock);
+        verify(latexSubSectionHeaderMock).write("References");
     }
 }

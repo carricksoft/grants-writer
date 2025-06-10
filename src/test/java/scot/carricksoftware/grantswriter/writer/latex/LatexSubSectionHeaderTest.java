af7639bc4e86fa4af57a6858e942e8384a6eddc9
Andrew Grant <carricksoft@gmail.com>
2025-06-10 11:26:27 +0100

    LatexSubSectionHeaderTest

--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/LatexSubSectionHeaderTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/LatexSubSectionHeaderTest.java
@@ -7,11 +7,15 @@ package scot.carricksoftware.grantswriter.writer.latex;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import scot.carricksoftware.grantswriter.writer.FileWriter;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static scot.carricksoftware.grantswriter.GenerateCertificateRandomValues.GetRandomString;
 
+@ExtendWith(MockitoExtension.class)
 class LatexSubSectionHeaderTest {
 
     private LatexSubSectionHeader header;
@@ -25,7 +29,10 @@ class LatexSubSectionHeaderTest {
     }
 
     @Test
-    void constructorTest() {
-        assertNotNull(header);
+    void writeTest() {
+        String title = GetRandomString();
+        header.write(title);
+        String requiredString = "\\subsection{" + title + "}";
+        verify(fileWriterMock).writeLine(requiredString);
     }
 }

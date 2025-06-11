e9861d82447b9dfc341439d214b1709d5161a388
Andrew Grant <carricksoft@gmail.com>
2025-06-11 10:06:23 +0100

    PersonSubSectionTimeLineWriterTest

--- src/test/java/scot/carricksoftware/grantswriter/data/TimelineDataAddTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/data/TimelineDataAddTest.java
@@ -48,7 +48,6 @@ public class TimelineDataAddTest {
 
         censusEntryList = new ArrayList<>();
         censusEntryList.add(censusEntry);
-
     }
 
     @Test
--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/PersonSubSectionTimeLineWriterTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/PersonSubSectionTimeLineWriterTest.java
@@ -11,11 +11,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import scot.carricksoftware.grantswriter.data.TimelineData;
+import scot.carricksoftware.grantswriter.domains.census.CensusEntry;
 import scot.carricksoftware.grantswriter.domains.people.Person;
 import scot.carricksoftware.grantswriter.services.censusentry.CensusEntryService;
 import scot.carricksoftware.grantswriter.writer.latex.LatexSubSectionHeader;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PersonSubSectionTimeLineWriterTest {
@@ -31,6 +37,7 @@ class PersonSubSectionTimeLineWriterTest {
     @Mock
     private TimelineData timelineDataMock;
 
+
     @Mock
     private Person personMock;
 
@@ -41,7 +48,12 @@ class PersonSubSectionTimeLineWriterTest {
 
     @Test
     void writeTest() {
+        List<CensusEntry> censusEntryList = new ArrayList<>();
+        censusEntryList.add(new CensusEntry());
+        when(censusEntryServiceMock.findAllByPerson(any())).thenReturn(censusEntryList);
         writer.write(personMock);
+
         verify(latexSubSectionHeaderMock).write("Timeline");
+        verify(timelineDataMock).add(censusEntryList);
     }
 }

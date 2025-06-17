a3d3ce1245e6b0baa0bb7e61b802f81940414b8d
Andrew Grant <carricksoft@gmail.com>
2025-06-17 02:19:22 +0100

    WriteTimeLineTest (2)

--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/WriteTimeLineTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/WriteTimeLineTest.java
@@ -12,14 +12,18 @@ import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import scot.carricksoftware.grantswriter.data.DMY;
+import scot.carricksoftware.grantswriter.data.DMYImpl;
 import scot.carricksoftware.grantswriter.writer.FileWriter;
 import scot.carricksoftware.grantswriter.writer.latex.LatexLongTabLeEnd;
 import scot.carricksoftware.grantswriter.writer.latex.LatexLongTableStart;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
 import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class WriteTimeLineTest {
@@ -49,5 +53,29 @@ class WriteTimeLineTest {
         inOrder.verify(latexLongTableStartMock).write("l l");
         inOrder.verify(latexLongTabLeEndMock).write();
     }
+
+    @Test
+    void writeInnerTestNoValues() {
+        TreeMap<DMY, List<String>> map = new TreeMap<>();
+        writeTimeLine.write(map);
+        verifyNoInteractions(fileWriterMock);
+    }
+
+    @Test
+    void writeInnerTestValues() {
+        TreeMap<DMY, List<String>> map = new TreeMap<>();
+        DMY dmy = new DMYImpl();
+        dmy.parse("25/01/1953");
+        List<String> stringList = new ArrayList<>();
+        String event = "Event";
+        stringList.add(event);
+        map.put(dmy, stringList);
+
+        String required = "25/01/1953&" + event + "\\\\";
+
+        writeTimeLine.write(map);
+        verify(fileWriterMock).writeLine(required);
+    }
+
 
 }

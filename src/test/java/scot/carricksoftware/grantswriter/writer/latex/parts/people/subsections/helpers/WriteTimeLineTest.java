81317f0b628c2c81ab3ce6c6e1be3f85e43ceccd
Andrew Grant <carricksoft@gmail.com>
2025-06-18 18:45:06 +0100

    WriteTheDataTest

--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/WriteTimeLineTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/WriteTimeLineTest.java
@@ -11,12 +11,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.data.DMY;
+import scot.carricksoftware.grantswriter.data.DMYImpl;
 import scot.carricksoftware.grantswriter.data.TimeLineData;
 import scot.carricksoftware.grantswriter.writer.FileWriter;
 import scot.carricksoftware.grantswriter.writer.latex.LatexLongTabLeEnd;
 import scot.carricksoftware.grantswriter.writer.latex.LatexLongTableStart;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static scot.carricksoftware.grantswriter.GenerateCertificateRandomValues.GetRandomString;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -51,6 +60,22 @@ class WriteTimeLineTest {
         InOrder inOrder = inOrder(latexLongTableStartMock, latexLongTabLeEndMock);
         inOrder.verify(latexLongTableStartMock).write("l l");
         inOrder.verify(latexLongTabLeEndMock).write();
+    }
+
+    @Test
+    void writeTheDataTest() {
+        String event = GetRandomString();
+
+        TreeMap<DMY, List<String>> map = new TreeMap<>();
+        DMY dmy = new DMYImpl();
+        dmy.parse("25/01/1953");
+        List<String> stringList = new ArrayList<>();
+        stringList.add(event);
+        map.put(dmy, stringList);
+        when(timeLineDataMock.getTimeLine()).thenReturn(map);
+        writeTimeLine.write();
+
+        verify(fileWriterMock).writeLine(dmy + "&" + event + "\\\\" );
     }
 
 

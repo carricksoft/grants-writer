0318dbf06085477e6d1b588bdd4fe7d90ff6a08a
Andrew Grant <carricksoft@gmail.com>
2025-06-18 16:42:56 +0100

    WriteTimeLineTest

--- src/main/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/WriteTimeLineImpl.java
+++ src/main/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/WriteTimeLineImpl.java
@@ -64,4 +64,6 @@ public class WriteTimeLineImpl implements WriteTimeLine {
             }
         }
     }
+
+
 }
--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/WriteTimeLineTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/WriteTimeLineTest.java
@@ -8,6 +8,7 @@ package scot.carricksoftware.grantswriter.writer.latex.parts.people.subsections.
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import scot.carricksoftware.grantswriter.data.TimeLineData;
@@ -15,7 +16,7 @@ import scot.carricksoftware.grantswriter.writer.FileWriter;
 import scot.carricksoftware.grantswriter.writer.latex.LatexLongTabLeEnd;
 import scot.carricksoftware.grantswriter.writer.latex.LatexLongTableStart;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.inOrder;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -45,8 +46,11 @@ class WriteTimeLineTest {
     }
 
     @Test
-    void ConstructorTest() {
-        assertNotNull(writeTimeLine);
+    void writeTest() {
+        writeTimeLine.write();
+        InOrder inOrder = inOrder(latexLongTableStartMock, latexLongTabLeEndMock);
+        inOrder.verify(latexLongTableStartMock).write("l l");
+        inOrder.verify(latexLongTabLeEndMock).write();
     }
 
 

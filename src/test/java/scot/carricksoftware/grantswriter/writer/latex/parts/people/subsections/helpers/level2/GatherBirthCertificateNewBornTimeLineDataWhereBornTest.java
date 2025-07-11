ada9efc89f132483e8a627fb7b373037ce6551f4
Andrew Grant <carricksoft@gmail.com>
2025-07-11 17:27:24 +0100

    GatherBirthCertificateNewBornTimeLineDataWhereBornTest

--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/level2/GatherBirthCertificateNewBornTimeLineDataWhereBornTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/level2/GatherBirthCertificateNewBornTimeLineDataWhereBornTest.java
@@ -10,56 +10,77 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.data.DMY;
 import scot.carricksoftware.grantswriter.data.TimeLineData;
 import scot.carricksoftware.grantswriter.domains.certificates.birthcertificate.BirthCertificate;
-import scot.carricksoftware.grantswriter.domains.people.Person;
+import scot.carricksoftware.grantswriter.domains.places.Place;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.TreeMap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertTrue;
 import static scot.carricksoftware.grantswriter.GenerateCertificateRandomValues.GetRandomString;
 import static scot.carricksoftware.grantswriter.GenerateRandomPeopleValues.GetRandomPerson;
+import static scot.carricksoftware.grantswriter.GenerateRandomPlaceValues.GetRandomPlace;
 
 @ExtendWith(MockitoExtension.class)
-class GatherBirthCertificateNewBornTimeLineDataTest {
+class GatherBirthCertificateNewBornTimeLineDataWhereBornTest {
 
     GatherBirthCertificateNewBornTimeLineData gatherBirthCertificateNewBornTimeLineData;
 
     @Mock
     private TimeLineData timelineDataMock;
 
-    @Mock
-    private Person personMock;
-
     private List<BirthCertificate> birthCertificates;
+
+    private TreeMap<DMY, List<String>> timeLine ;
+
+    private BirthCertificate birthCertificate;
 
 
     @BeforeEach
     void setUp() {
         gatherBirthCertificateNewBornTimeLineData = new GatherBirthCertificateNewBornTimeLineDataImpl(this.timelineDataMock);
         birthCertificates = new ArrayList<>();
+
+        birthCertificate = new BirthCertificate();
+        setUpBirthCertificate(birthCertificate);
+        timeLine = new TreeMap<>();
+
     }
 
-    @Test
-    void refsAreAddedTest() {
-        SortedSet<String> refs = new TreeSet<>();
-        when(timelineDataMock.getRefs()).thenReturn(refs);
-        BirthCertificate birthCertificate = new BirthCertificate();
-        birthCertificate.setNewBorn(personMock);
+    private void setUpBirthCertificate(BirthCertificate birthCertificate) {
         birthCertificate.setWhenBorn("25/01/1953 01:01");
         birthCertificate.setWhenRegistered("25/01/1953 01:01");
         birthCertificate.setMother(GetRandomPerson());
         birthCertificate.setFather(GetRandomPerson());
+    }
+
+    @Test
+    void whereBornTest() {
+        Place place = GetRandomPlace();
+        birthCertificate.setWhereBorn(place);
         birthCertificates.add(birthCertificate);
-        String toString = GetRandomString();
-        when(personMock.toString()).thenReturn(toString);
+
+        when(timelineDataMock.getTimeLine()).thenReturn(timeLine);
 
         gatherBirthCertificateNewBornTimeLineData.gather(birthCertificates);
-        assertEquals("Birth Certificate for : " + toString, refs.first());
+        String expected = "Born at " + place;
+        assertTrue(timeLine.firstEntry().getValue().contains(expected));
+    }
 
+    @Test
+    void whereBornUntrackedTest() {
+        String untrackedPlace = GetRandomString();
+        birthCertificate.setUntrackedWhereBorn(untrackedPlace);
+        birthCertificates.add(birthCertificate);
+
+        when(timelineDataMock.getTimeLine()).thenReturn(timeLine);
+
+        gatherBirthCertificateNewBornTimeLineData.gather(birthCertificates);
+        String expected = "Born at " + untrackedPlace;
+        assertTrue(timeLine.firstEntry().getValue().contains(expected));
     }
 }

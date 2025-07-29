db5fbf8174538e664487b8e89a97f36749b79501
Andrew Grant <carricksoft@gmail.com>
2025-07-29 08:40:27 +0100

    DeathCertificateServiceTest::findByAllInformantTest

---
+++ src/test/java/scot/carricksoftware/grantswriter/services/certificates/deathcertificate/DeathCertificateServiceTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grantswriter.services.certificates.deathcertificate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.domains.certificates.deathcertificate.DeathCertificate;
+import scot.carricksoftware.grantswriter.domains.people.Person;
+import scot.carricksoftware.grantswriter.repositories.certificates.deathcertificate.DeathCertificateRepository;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static scot.carricksoftware.grantswriter.GenerateRandomPeopleValues.GetRandomPerson;
+
+@ExtendWith(MockitoExtension.class)
+class DeathCertificateServiceTest {
+
+    private DeathCertificateService deathCertificateService;
+    private List<DeathCertificate> deathCertificateList;
+
+    private Person person;
+
+    @Mock
+    DeathCertificateRepository deathCertificateRepositoryMock;
+
+    @BeforeEach
+    void setUp() {
+        deathCertificateService = new DeathCertificateServiceImpl(deathCertificateRepositoryMock);
+        deathCertificateList = new ArrayList<>();
+        DeathCertificate deathCertificate = new DeathCertificate();
+        deathCertificateList.add(deathCertificate);
+        person = GetRandomPerson();
+    }
+
+    @Test
+    void findAllByInformantTest() {
+        when(deathCertificateRepositoryMock.findAllByInformant(person)).thenReturn(deathCertificateList);
+
+        List<DeathCertificate> result = deathCertificateService.findAllByInformant(person);
+        assertEquals(deathCertificateList, result);
+    }
+}

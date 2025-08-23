24a88c6b4d8b87d22a41bbcd3a5b5f188dd81665
Andrew Grant <carricksoft@gmail.com>
2025-08-23 10:49:03 +0100

    GatherMarriageCertificateBrideTimeWhenMarriedTest

---
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/helpers/level2/marriagecertificates/GatherMarriageCertificateBrideTimeLineDataWhenMarriedTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grantswriter.writer.latex.parts.people.subsections.helpers.level2.marriagecertificates;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.data.DMY;
+import scot.carricksoftware.grantswriter.data.TimeLineData;
+import scot.carricksoftware.grantswriter.domains.certificates.marriagecertificate.MarriageCertificate;
+import scot.carricksoftware.grantswriter.domains.people.Person;
+import scot.carricksoftware.grantswriter.domains.places.Place;
+import scot.carricksoftware.grantswriter.writer.latex.parts.people.subsections.helpers.level2.marriagecertificate.GatherMarriageCertificateBrideTimeLineData;
+import scot.carricksoftware.grantswriter.writer.latex.parts.people.subsections.helpers.level2.marriagecertificate.GatherMarriageCertificateBrideTimeLineDataImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertTrue;
+import static scot.carricksoftware.grantswriter.GenerateCertificateRandomValues.GetRandomString;
+import static scot.carricksoftware.grantswriter.GenerateRandomPeopleValues.GetRandomPerson;
+import static scot.carricksoftware.grantswriter.GenerateRandomPlaceValues.GetRandomPlace;
+
+@ExtendWith(MockitoExtension.class)
+class GatherMarriageCertificateBrideTimeLineDataWhenMarriedTest {
+
+    GatherMarriageCertificateBrideTimeLineData gatherMarriageCertificateBrideTimeLineData;
+
+    @Mock
+    private TimeLineData timelineDataMock;
+
+    private List<MarriageCertificate> marriageCertificates;
+
+    private TreeMap<DMY, List<String>> timeLine;
+
+    private Person bride;
+
+    private Person groom;
+
+    private Place whereMarried;
+
+    private String untrackedPlace;
+
+    private MarriageCertificate marriageCertificate;
+
+
+    @BeforeEach
+    void setUp() {
+        gatherMarriageCertificateBrideTimeLineData = new GatherMarriageCertificateBrideTimeLineDataImpl(this.timelineDataMock);
+        marriageCertificates = new ArrayList<>();
+
+        marriageCertificate = new MarriageCertificate();
+        timeLine = new TreeMap<>();
+        bride = GetRandomPerson();
+        groom = GetRandomPerson();
+        whereMarried = GetRandomPlace();
+        untrackedPlace = GetRandomString();
+        setUpBaseCertificate();
+
+    }
+
+    private void setUpBaseCertificate() {
+        marriageCertificate.setWhenMarried("25/01/1953");
+        marriageCertificate.setBride(bride);
+        marriageCertificate.setGroom(groom);
+        marriageCertificate.setWhereMarried(whereMarried);
+
+    }
+
+    private void setUpTrackedCertificate() {
+        setUpBaseCertificate();
+        marriageCertificate.setWhereMarried(whereMarried);
+        marriageCertificates.add(marriageCertificate);
+    }
+
+    private void setUpUntrackedCertificate() {
+        setUpBaseCertificate();
+        marriageCertificate.setWhereMarried(null);
+        marriageCertificate.setUntrackedWhereMarried(untrackedPlace);
+        marriageCertificates.add(marriageCertificate);
+    }
+
+    @Test
+    void whereMarriedTest() {
+        setUpTrackedCertificate();
+        when(timelineDataMock.getTimeLine()).thenReturn(timeLine);
+
+        gatherMarriageCertificateBrideTimeLineData.gather(marriageCertificates);
+        String expected = "Married " + groom.toString() + " at " + whereMarried.toString();
+        assertTrue(timeLine.firstEntry().getValue().contains(expected));
+    }
+
+    @Test
+    void untrackedWhereMarriedTest() {
+        setUpUntrackedCertificate();
+        when(timelineDataMock.getTimeLine()).thenReturn(timeLine);
+
+        gatherMarriageCertificateBrideTimeLineData.gather(marriageCertificates);
+        String expected = "Married " + groom.toString() + " at " + untrackedPlace;
+        assertTrue(timeLine.firstEntry().getValue().contains(expected));
+    }
+
+
+
+}

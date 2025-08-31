8825037e7bf62a75e6622c07595bcd8a5cfdd7c7
Andrew Grant <carricksoft@gmail.com>
2025-08-31 15:31:24 +0100

    Eliminate unused

--- src/main/java/scot/carricksoftware/grantswriter/constants/ApplicationConstants.java
+++ src/main/java/scot/carricksoftware/grantswriter/constants/ApplicationConstants.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.format.DateTimeFormatter;
 
-@SuppressWarnings("unused")
+
 @Component
 public class ApplicationConstants {
 
@@ -17,13 +17,9 @@ public class ApplicationConstants {
         // to stop checkstyle complaining
     }
 
-    public static final int DEFAULT_PAGE_SIZE = 15;
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     public static final String DATE_FORMAT = "dd/MM/yyyy";
     public static final String DATE_TIME_FORMAT = "dd/MM/yyyy hh:mm";
     public final static String EMPTY_STRING = "";
-
-    public static final int MINIMUM_NAME_LENGTH = 3;
-    public static final int MAXIMUM_NAME_LENGTH = 40;
 
 }

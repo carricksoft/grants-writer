d05b42dd851575105a13ff9738212c4a9119793c
Andrew Grant <carricksoft@gmail.com>
2025-10-12 16:36:44 +0100

    StringToFileConverterTest FileIsCreated Test

--- src/test/java/scot/carricksoftware/grantswriter/converters/StringToFileConverterTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/converters/StringToFileConverterTest.java
@@ -9,7 +9,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.bind.DatatypeConverter;
+
 import java.io.File;
+import java.io.FileInputStream;
 
 import static org.codehaus.plexus.util.FileUtils.fileExists;
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,11 +40,38 @@ class StringToFileConverterTest {
         assertThrows(Exception.class, () -> converter.convert(GetRandomString(), null));
     }
 
+    @Test
+    void aCorrectFileIsCreatedTest() {
+        String testString = "Hello World!";
+        byte[] testBytes = DatatypeConverter.parseBase64Binary(testString);
+        converter.convert(testString, fileName);
+
+        byte[] result = readFileToBytes();
+        assertArrayEquals(testBytes, result);
+    }
+
+
     @SuppressWarnings("AssertWithSideEffects")
     @AfterEach
     void tearDown() {
         File f1 = new File(fileName);
         assert !f1.exists() || (f1.delete());
+    }
+
+    private byte[] readFileToBytes() {
+// from https://stackoverflow.com/questions/858980/file-to-byte-in-java
+        File file = new File(fileName);
+        byte[] bytes = new byte[(int) file.length()];
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            //noinspection ResultOfMethodCallIgnored
+            fis.read(bytes);
+            return bytes;
+        } catch (Exception ignored) {
+
+        }
+        return null;
+
     }
 
 

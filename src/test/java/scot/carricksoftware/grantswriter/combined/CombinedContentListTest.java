6802de6777abb4c06d16c7014aad28ba54deeaae
Andrew Grant <carricksoft@gmail.com>
2025-10-15 23:04:37 +0100

    CombinedContentListTest (2)

--- src/test/java/scot/carricksoftware/grantswriter/combined/CombinedContentListTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/combined/CombinedContentListTest.java
@@ -8,6 +8,7 @@ package scot.carricksoftware.grantswriter.combined;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import scot.carricksoftware.grantswriter.domains.images.PersonImage;
+import scot.carricksoftware.grantswriter.domains.text.PersonText;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static scot.carricksoftware.grantswriter.GenerateCertificateRandomValues.GetRandomString;
@@ -30,8 +31,34 @@ class CombinedContentListTest {
 
         combinedContentList.addPersonImage(image);
 
-        assertEquals(image.getOrder(), combinedContentList.getList().get(0).getOrder(), image.getOrder());
+        assertEquals(image.getOrder(), combinedContentList.getList().get(0).getOrder());
         assertEquals("image", combinedContentList.getList().get(0).getContentType());
         assertEquals(image.getId(), combinedContentList.getList().get(0).getContentId());
+    }
+
+    @Test
+    void addPersonTextTest() {
+        PersonText text = new PersonText();
+        text.setOrder(GetRandomString());
+        text.setId(GetRandomLong());
+
+        combinedContentList.addPersonText(text);
+
+        assertEquals(text.getOrder(), combinedContentList.getList().get(0).getOrder());
+        assertEquals("text", combinedContentList.getList().get(0).getContentType());
+        assertEquals(text.getId(), combinedContentList.getList().get(0).getContentId());
+    }
+
+    @Test
+    void clearTest() {
+        PersonText text = new PersonText();
+        text.setOrder(GetRandomString());
+        text.setId(GetRandomLong());
+
+        combinedContentList.addPersonText(text);
+
+        assertEquals(1, combinedContentList.getList().size());
+        combinedContentList.clear();
+        assertEquals(0, combinedContentList.getList().size());
     }
 }

0a496b4118bca0b0ce12fdef2bc6f89fe5b1b2dc
Andrew Grant <carricksoft@gmail.com>
2025-10-16 14:06:33 +0100

    PersonImageRepository

---
+++ src/main/java/scot/carricksoftware/grantswriter/repositories/images/PersonImageRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Andrew Grant of Carrick Software 28/03/2025, 11:02. All rights reserved.
+ *
+ */
+
+package scot.carricksoftware.grantswriter.repositories.images;
+
+
+import scot.carricksoftware.grantswriter.domains.images.PersonImage;
+import scot.carricksoftware.grantswriter.domains.people.Person;
+import scot.carricksoftware.grantswriter.repositories.ReadOnlyRepository;
+
+
+public interface PersonImageRepository extends ReadOnlyRepository<PersonImage, Long> {
+    Iterable<PersonImage> findAllByPerson(Person person);
+}

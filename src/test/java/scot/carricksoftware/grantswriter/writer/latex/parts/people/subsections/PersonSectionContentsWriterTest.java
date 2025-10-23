1d8604fccfa4fa7f9c3fae7e16cad37f1172b9f9
Andrew Grant <carricksoft@gmail.com>
2025-10-23 08:55:43 +0100

    PersonSectionContentsWriterTest

--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/PersonSectionContentsWriterTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/people/subsections/PersonSectionContentsWriterTest.java
@@ -10,12 +10,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.combined.Combined;
+import scot.carricksoftware.grantswriter.combined.CombinedContentList;
+import scot.carricksoftware.grantswriter.combined.CombinedImpl;
+import scot.carricksoftware.grantswriter.domains.people.Person;
+import scot.carricksoftware.grantswriter.domains.text.PersonText;
 import scot.carricksoftware.grantswriter.services.combined.CombinedService;
 import scot.carricksoftware.grantswriter.services.text.PersonTextService;
 import scot.carricksoftware.grantswriter.writer.FileWriter;
 import scot.carricksoftware.grantswriter.writer.latex.LatexDivisionHeader;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static scot.carricksoftware.grantswriter.GenerateCertificateRandomValues.GetRandomString;
 
 @ExtendWith(MockitoExtension.class)
 class PersonSectionContentsWriterTest {
@@ -26,6 +36,7 @@ class PersonSectionContentsWriterTest {
     @Mock private CombinedService combinedServiceMock;
     @Mock private FileWriter fileWriterMock;
     @Mock private LatexDivisionHeader latexDivisionHeaderMock;
+    @Mock private CombinedContentList combinedContentListMock;
 
     @BeforeEach
     void setUp() {
@@ -37,8 +48,28 @@ class PersonSectionContentsWriterTest {
                 );
     }
 
+
     @Test
-    void constructorTest() {
-        assertNotNull(writer);
+    void thePersonTextIsWrittenTest() {
+        Person person = new Person();
+        Combined combined = new CombinedImpl();
+        combined.setContentId(1L);
+        combined.setContentType("text");
+        String content = GetRandomString();
+        String heading = GetRandomString();
+        String level = "99";
+        PersonText personText = new PersonText();
+        personText.setContent(content);
+        personText.setHeading(heading);
+        personText.setLevel(level);
+        List<Combined> combinedList = new ArrayList<>();
+        combinedList.add(combined);
+        when(combinedServiceMock.getPersonContent(person)).thenReturn(combinedContentListMock);
+        when(combinedContentListMock.getList()).thenReturn(combinedList);
+        when(personTextServiceMock.findById(1L)).thenReturn(personText);
+        writer.write(person);
+        verify(latexDivisionHeaderMock).write(99, heading);
+        verify(fileWriterMock).writeLine(content);
     }
+
 }

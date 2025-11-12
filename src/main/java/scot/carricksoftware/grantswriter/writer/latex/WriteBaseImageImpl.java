feaba1456ca015b4fb9ae7d32f3de253e2d01113
apg <carricksoft@gmail.com>
2025-11-12 21:39:42 +0000

    Height added, width changed to max with

--- src/main/java/scot/carricksoftware/grantswriter/writer/latex/WriteBaseImageImpl.java
+++ src/main/java/scot/carricksoftware/grantswriter/writer/latex/WriteBaseImageImpl.java
@@ -32,8 +32,7 @@ public class WriteBaseImageImpl implements WriteBaseImage {
     public void write(BaseImage baseImage) {
         createActualImage(baseImage.getImage());
         latexBlock.begin("center", "");
-        //noinspection SpellCheckingInspection
-        fileWriter.writeLine("\\includegraphics[width=0.25\\linewidth]" +
+        fileWriter.writeLine("\\includegraphics[max width=0.25\\linewidth, max height=0.25\\linewidth,]" +
                 LatexConstants.TERM_START +
                 ApplicationConstants.TEMP_DIRECTORY  +
                 baseImage.getImage().getFileName() +

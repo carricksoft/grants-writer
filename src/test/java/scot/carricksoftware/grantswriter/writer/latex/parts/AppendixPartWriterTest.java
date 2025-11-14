0a538955caa5b709c8a088b59b179bcf3514be2f
apg <carricksoft@gmail.com>
2025-11-14 20:06:32 +0000

    AppendixPartWriterTest

--- src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/AppendixPartWriterTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/AppendixPartWriterTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.combined.*;
+import scot.carricksoftware.grantswriter.domains.images.AppendixImage;
+import scot.carricksoftware.grantswriter.domains.text.AppendixText;
 import scot.carricksoftware.grantswriter.services.combined.CombinedService;
 import scot.carricksoftware.grantswriter.services.image.AppendixImageService;
 import scot.carricksoftware.grantswriter.services.text.AppendixTextService;
@@ -19,19 +22,37 @@ import scot.carricksoftware.grantswriter.writer.latex.parts.appendix.AppendixPar
 import scot.carricksoftware.grantswriter.writer.latex.parts.appendix.AppendixPartWriterImpl;
 import scot.carricksoftware.grantswriter.writer.latex.parts.appendix.headers.AppendixPartHeader;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AppendixPartWriterTest {
 
     private AppendixPartWriter appendixPartWriter;
 
-    @Mock private AppendixPartHeader appendixPartHeaderMock;
-    @Mock private CombinedService combinedServiceMock;
-    @Mock private WriteBaseText writeBaseTextMock;
-    @Mock private WriteBaseImage writeBaseImageMock;
-    @Mock private AppendixTextService appendixTextServiceMock;
-    @Mock private AppendixImageService appendixImageServiceMock;
+    @Mock
+    private AppendixPartHeader appendixPartHeaderMock;
+    @Mock
+    private CombinedService combinedServiceMock;
+    @Mock
+    private WriteBaseText writeBaseTextMock;
+    @Mock
+    private WriteBaseImage writeBaseImageMock;
+    @Mock
+    private AppendixTextService appendixTextServiceMock;
+    @Mock
+    private AppendixImageService appendixImageServiceMock;
+
+    @Mock
+    private CombinedContentList combinedContentListMock;
+
+    private final List<Combined> combinedList = new ArrayList<>();
+
+    private Combined combined;
 
     @BeforeEach
     void setUp() {
@@ -42,10 +63,31 @@ class AppendixPartWriterTest {
                 writeBaseImageMock,
                 appendixTextServiceMock,
                 appendixImageServiceMock);
+        combined = new CombinedImpl();
+        when(combinedServiceMock.getAppendixContent()).thenReturn(combinedContentListMock);
     }
 
     @Test
-    void constructorTest() {
-        assertNotNull(appendixPartWriter);
+    void imagesAreCorrectlyRoutedTest() {
+        combined.setContentType("image");
+        combinedList.add(combined);
+        AppendixImage appendixImage = new AppendixImage();
+        when(combinedContentListMock.getList()).thenReturn(combinedList);
+        when(appendixImageServiceMock.findById(any())).thenReturn(appendixImage);
+
+        appendixPartWriter.write();
+        verify(writeBaseImageMock).write(appendixImage);
+    }
+
+    @Test
+    void textsAreCorrectlyRoutedTest() {
+        combined.setContentType("text");
+        combinedList.add(combined);
+        AppendixText appendixText = new AppendixText();
+        when(combinedContentListMock.getList()).thenReturn(combinedList);
+        when(appendixTextServiceMock.findById(any())).thenReturn(appendixText);
+
+        appendixPartWriter.write();
+        verify(writeBaseTextMock).write(appendixText);
     }
 }

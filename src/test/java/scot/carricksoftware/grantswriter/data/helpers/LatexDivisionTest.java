7d4ee6907ff53e65fc21e0255fabe1e9ecf31c23
apg <carricksoft@gmail.com>
2025-11-24 14:10:56 +0000

    LatexDivisionTest

--- src/test/java/scot/carricksoftware/grantswriter/data/helpers/LatexDivisionTest.java
+++ src/test/java/scot/carricksoftware/grantswriter/data/helpers/LatexDivisionTest.java
@@ -8,39 +8,17 @@ package scot.carricksoftware.grantswriter.data.helpers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static scot.carricksoftware.grantswriter.constants.LatexLevels.LATEX_BOOK;
-import static scot.carricksoftware.grantswriter.constants.LatexLevels.LATEX_CHAPTER;
-import static scot.carricksoftware.grantswriter.constants.LatexLevels.LATEX_PARAGRAPH;
-import static scot.carricksoftware.grantswriter.constants.LatexLevels.LATEX_PART;
-import static scot.carricksoftware.grantswriter.constants.LatexLevels.LATEX_SECTION;
 import static scot.carricksoftware.grantswriter.constants.LatexLevels.LATEX_SUBPARAGRAPH;
-import static scot.carricksoftware.grantswriter.constants.LatexLevels.LATEX_SUBSECTION;
-import static scot.carricksoftware.grantswriter.constants.LatexLevels.LATEX_SUB_SUBSECTION;
 
-@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
 class LatexDivisionTest {
 
     private LatexDivision latexDivision;
 
-    @SuppressWarnings("FieldCanBeLocal")
-    private Map<Integer, String> headers;
-
     @BeforeEach
     void setUp() {
         latexDivision = new LatexDivisionImpl();
-        headers = new HashMap<>();
-        headers.put(LATEX_BOOK, "\\book{");
-        headers.put(LATEX_PART, "\\part{");
-        headers.put(LATEX_CHAPTER, "\\chapter{");
-        headers.put(LATEX_SECTION, "\\section{");
-        headers.put(LATEX_SUBSECTION, "\\subsection{");
-        headers.put(LATEX_SUB_SUBSECTION, "\\subsubsection{");
-        headers.put(LATEX_PARAGRAPH, "\\paragraph{");
-        headers.put(LATEX_SUBPARAGRAPH, "\\subparagraph{");
     }
 
     @Test
@@ -51,5 +29,10 @@ class LatexDivisionTest {
     @Test
     void highLevelThrowsAnExceptionTest() {
         assertThrows(RuntimeException.class, () -> latexDivision.header(LATEX_SUBPARAGRAPH + 1));
+    }
+
+    @Test
+    void sectionTest() {
+        assertEquals("\\section", latexDivision.header(1));
     }
 }

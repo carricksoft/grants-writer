5a9b6e0cd66bc7d98be48375ac3b7c9874676460
apg <carricksoft@gmail.com>
2026-02-05 00:23:25 +0000

    LatexItemiseEnd

---
+++ src/main/java/scot/carricksoftware/grantswriter/writer/latex/LatexItemizeEndImpl.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grantswriter.writer.latex;
+
+import org.springframework.stereotype.Component;
+import scot.carricksoftware.grantswriter.constants.LatexConstants;
+
+
+@Component
+public class LatexItemizeEndImpl implements LatexItemizeEnd {
+
+    private final LatexBlock latexBlock;
+
+    public LatexItemizeEndImpl(LatexBlock latexBlock) {
+        this.latexBlock = latexBlock;
+    }
+
+    @Override
+    public void write() {
+        latexBlock.end(LatexConstants.ITEMIZE);
+    }
+}

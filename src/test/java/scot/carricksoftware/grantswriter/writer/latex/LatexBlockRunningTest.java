2e8642a7c698b672e20a7513a33ca759e6da6928
apg <carricksoft@gmail.com>
2026-02-05 17:04:07 +0000

    LatexBlockRunningTest

---
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/LatexBlockRunningTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grantswriter.writer.latex;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.writer.FileWriter;
+
+import static org.mockito.Mockito.verify;
+import static scot.carricksoftware.grantswriter.GenerateCertificateRandomValues.GetRandomString;
+
+@ExtendWith(MockitoExtension.class)
+class LatexBlockRunningTest {
+
+    private LatexBlock latexBlock;
+
+    @Mock
+    private FileWriter fileWriterMock;
+
+    private String blockName;
+
+    @BeforeEach
+    void setUp() {
+        latexBlock = new LatexBlockImpl(fileWriterMock);
+        blockName = GetRandomString();
+    }
+
+    @Test
+    void endRunningRunningBlockTest() {
+        String expected = "\\end{" + blockName + "}";
+        latexBlock.endRunningBlock(blockName);
+        verify(fileWriterMock).writeLine(expected);
+    }
+
+    @Test
+    void beginRunningBlockTest() {
+        String expected = "\\begin{" + blockName + "}";
+        latexBlock.beginRunningBlock(blockName, null);
+        verify(fileWriterMock).writeLine(expected);
+    }
+}

d1944aa6f9e2ed8579d053790f3fa121f271f74f
apg <carricksoft@gmail.com>
2026-02-10 00:15:52 +0000

    PlaceSectionContentsWriter

---
+++ src/main/java/scot/carricksoftware/grantswriter/writer/latex/parts/places/subsections/PlaceSectionContentsWriter.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grantswriter.writer.latex.parts.places.subsections;
+
+import scot.carricksoftware.grantswriter.domains.places.Place;
+
+public interface PlaceSectionContentsWriter {
+    void write(Place place);
+}
---
+++ src/main/java/scot/carricksoftware/grantswriter/writer/latex/parts/places/subsections/PlaceSectionContentsWriterImpl.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grantswriter.writer.latex.parts.places.subsections;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import scot.carricksoftware.grantswriter.domains.places.Place;
+import scot.carricksoftware.grantswriter.services.combined.CombinedService;
+import scot.carricksoftware.grantswriter.writer.latex.WriteBaseImage;
+import scot.carricksoftware.grantswriter.writer.latex.WriteBaseText;
+
+@Component
+public class PlaceSectionContentsWriterImpl implements PlaceSectionContentsWriter {
+
+    private static final Logger logger = LogManager.getLogger(PlaceSectionContentsWriterImpl.class);
+
+ //   private final PersonTextService personTextService;
+ //   private final PersonImageService personImageService;
+    private final CombinedService combinedService;
+    private final WriteBaseText writeBaseText;
+    private final WriteBaseImage writeBaseImage;
+
+    public PlaceSectionContentsWriterImpl(CombinedService combinedService, WriteBaseText writeBaseText, WriteBaseImage writeBaseImage) {
+        this.combinedService = combinedService;
+        this.writeBaseText = writeBaseText;
+        this.writeBaseImage = writeBaseImage;
+    }
+
+    @Override
+    public void write(Place place) {
+       logger.info("PersonSectionContentsWriterImpl.write()");
+//        List<Combined> combinedList = combinedService.getPlaceContent(place).getList();
+//        for (Combined combined : combinedList) {
+//            if (combined.getContentType().equals(CombinedContentType.TEXT.label)) {
+//                writeBaseText.write(personTextService.findById(combined.getContentId()));
+//            }
+//            else {
+//                if (combined.getContentType().equals(CombinedContentType.IMAGE.label)) {
+//                    writeBaseImage.write(personImageService.findById(combined.getContentId()));
+//                }
+//            }
+//       }
+    }
+
+}

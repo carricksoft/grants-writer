6a18d6bebcb7b2aae88dfab0e16d54c424908292
apg <carricksoft@gmail.com>
2026-02-20 23:23:04 +0000

    GatherPlaceBirthCertificateTimeLineDataTest

---
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/places/subsections/helpers/GatherPlaceBirthCertificateTimeLineDataTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2026.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grantswriter.writer.latex.parts.places.subsections.helpers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.domains.certificates.birthcertificate.BirthCertificate;
+import scot.carricksoftware.grantswriter.domains.places.Place;
+import scot.carricksoftware.grantswriter.services.certificates.birthcertificate.BirthCertificateService;
+import scot.carricksoftware.grantswriter.writer.latex.parts.places.subsections.helpers.level2.birthcertificate.GatherPlaceBirthCertificateWhereBornTimeLineData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static scot.carricksoftware.grantswriter.GenerateRandomPlaceValues.GetRandomPlace;
+
+@ExtendWith(MockitoExtension.class)
+class GatherPlaceBirthCertificateTimeLineDataTest {
+
+    private GatherPlaceBirthCertificateTimeLineData gatherPlaceBirthCertificateTimeLineData;
+
+    @Mock
+    private BirthCertificateService birthCertificateServiceMock;
+    @Mock
+    private GatherPlaceBirthCertificateWhereBornTimeLineData gatherPlaceBirthCertificateNewBornTimeLineDataMock;
+
+    List<BirthCertificate> birthCertificates;
+    Place place;
+
+    @BeforeEach
+    void setUp() {
+        gatherPlaceBirthCertificateTimeLineData = new GatherPlaceBirthCertificateTimeLineDataImpl(
+                birthCertificateServiceMock,
+                gatherPlaceBirthCertificateNewBornTimeLineDataMock);
+        birthCertificates = new ArrayList<>();
+
+        place = GetRandomPlace();
+    }
+
+    @Test
+    void emptyTest() {
+        List<BirthCertificate> birthCertificates = new ArrayList<>();
+        when(birthCertificateServiceMock.findAllByWhereBorn(place)).thenReturn(birthCertificates);
+        gatherPlaceBirthCertificateTimeLineData.gather(place);
+        verifyNoInteractions(gatherPlaceBirthCertificateNewBornTimeLineDataMock);
+    }
+
+    @Test
+    void nonEmptyTest() {
+        List<BirthCertificate> birthCertificates = new ArrayList<>();
+        birthCertificates.add(new BirthCertificate());
+        when(birthCertificateServiceMock.findAllByWhereBorn(place)).thenReturn(birthCertificates);
+        gatherPlaceBirthCertificateTimeLineData.gather(place);
+        verify(gatherPlaceBirthCertificateNewBornTimeLineDataMock).gather(birthCertificates);
+    }
+
+}

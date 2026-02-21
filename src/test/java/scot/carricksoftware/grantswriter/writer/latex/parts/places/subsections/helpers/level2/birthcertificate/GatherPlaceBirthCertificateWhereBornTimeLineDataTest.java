c90611dee7781f3268d931b7ad358d5f7331178b
apg <carricksoft@gmail.com>
2026-02-21 19:00:41 +0000

    GatherPlaceBirthCertificateWhereBornTimeLineDataTest partial

---
+++ src/test/java/scot/carricksoftware/grantswriter/writer/latex/parts/places/subsections/helpers/level2/birthcertificate/GatherPlaceBirthCertificateWhereBornTimeLineDataTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grantswriter.writer.latex.parts.places.subsections.helpers.level2.birthcertificate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grantswriter.data.DMY;
+import scot.carricksoftware.grantswriter.data.TimeLineData;
+import scot.carricksoftware.grantswriter.domains.certificates.birthcertificate.BirthCertificate;
+import scot.carricksoftware.grantswriter.domains.people.Person;
+import scot.carricksoftware.grantswriter.domains.places.Place;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertTrue;
+import static scot.carricksoftware.grantswriter.GenerateRandomPeopleValues.GetRandomPerson;
+import static scot.carricksoftware.grantswriter.GenerateRandomPlaceValues.GetRandomPlace;
+
+@ExtendWith(MockitoExtension.class)
+class GatherPlaceBirthCertificateWhereBornTimeLineDataTest {
+
+    GatherPlaceBirthCertificateWhereBornTimeLineData gatherPlaceBirthCertificateWhereBornTimeLineData;
+
+    @Mock
+    private TimeLineData timelineDataMock;
+
+    private List<BirthCertificate> birthCertificates;
+
+    private TreeMap<DMY, List<String>> timeLine;
+
+    private BirthCertificate birthCertificate;
+
+    private Person person;
+
+    @BeforeEach
+    void setUp() {
+        gatherPlaceBirthCertificateWhereBornTimeLineData = new GatherPlaceBirthCertificateWhereBornTimeLineDataImpl(this.timelineDataMock);
+        person = GetRandomPerson();
+        birthCertificates = new ArrayList<>();
+
+        birthCertificate = new BirthCertificate();
+        setUpBirthCertificate(birthCertificate);
+        timeLine = new TreeMap<>();
+
+    }
+
+    private void setUpBirthCertificate(BirthCertificate birthCertificate) {
+        birthCertificate.setWhereBorn(GetRandomPlace());
+        birthCertificate.setWhenBorn("25/02/1953 01:01");
+        birthCertificate.setNewBorn(person);
+    }
+
+    @Test
+    void whereBornTest() {
+        Place place = GetRandomPlace();
+        birthCertificate.setWhereBorn(place);
+        birthCertificates.add(birthCertificate);
+        when(timelineDataMock.getTimeLine()).thenReturn(timeLine);
+
+        String required = person.toString() + " Born here.";
+        gatherPlaceBirthCertificateWhereBornTimeLineData.gather(birthCertificates);
+        assertTrue(timeLine.firstEntry().getValue().contains(required));
+    }
+
+
+}
